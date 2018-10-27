b6092d22d0294fb2f5070dc27d0bb9772f68832d
Rinni Swift <rinradaswift@gmail.com>
2018-10-27 10:45:45 -0700

    complete 8 ball random answer generator

--- magicEightBall/ViewController.swift
+++ magicEightBall/ViewController.swift
@@ -10,9 +10,13 @@ import UIKit
 
 class ViewController: UIViewController {
     
+    let answers = ["Yes, definitely", "It is certain", "Without a doubt", "Yes", "Most likely", "Sure, why not?", "Same", "Tell me more", "Out to lunch", "Reply hazy, try again", "Ask again later", "The cake is a lie", "42", "TMI", "Very doubtful", "Don't count on it", "My reply is no", "Absolutely not"]
+    
     @IBOutlet weak var answerLabel: UILabel!
     @IBOutlet weak var shakeButton: UIButton!
 
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         
@@ -20,10 +24,20 @@ class ViewController: UIViewController {
         
     }
     
+    override func motionEnded(_ motion: UIEvent.EventSubtype, with event: UIEvent?) {
+        guard motion == .motionShake else { return }
+        generateAnswers()
+    }
+    
     @IBAction func shakeButtonTapped(_ sender: Any) {
-        
+        generateAnswers()
     }
 
-
+    func generateAnswers() {
+        let indexes = UInt32(answers.count)
+        let randomIndex = Int(arc4random_uniform(indexes))
+        
+        answerLabel.text = answers[randomIndex]
+    }
 }
 
