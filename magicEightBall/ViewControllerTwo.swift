d6998c4f0c4be5d96f26af128ba2c510164fd38b
Rinni Swift <rinradaswift@gmail.com>
2018-10-27 11:16:08 -0700

    added tab bar controller with fully functioned random answer generator and idea generator with only constraints

---
+++ magicEightBall/ViewControllerTwo.swift
@@ -0,0 +1,21 @@
+//
+//  ViewControllerTwo.swift
+//  magicEightBall
+//
+//  Created by Rinni Swift on 10/27/18.
+//  Copyright © 2018 Rinni Swift. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+class ViewControllerTwo: UIViewController {
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        view.gradientBG(colorOne: .lightPink, colorTwo: .lightPurple)
+        
+    }
+    
+}
