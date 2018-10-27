9f94caff43de57d80bcb062a6466a78d316c21c7
Rinni Swift <rinradaswift@gmail.com>
2018-10-27 11:40:20 -0700

    fully functioned idea generator and random answer generator

--- magicEightBall/ViewControllerTwo.swift
+++ magicEightBall/ViewControllerTwo.swift
@@ -11,11 +11,32 @@ import UIKit
 
 class ViewControllerTwo: UIViewController {
     
+    let startups = ["Make School", "Uber", "Netflix", "Facebook", "Google", "Kickstarter", "Spotify", "Airbnb", "Snapchat", "YouTube", "Dropbox", "Amazon", "Craigslist", "Tinder", "Instagram", "Tesla", "Twitter", "SpaceX"]
+    let ideas = ["Dogs", "Books", "Gamers", "Star Wars", "Bitcoin", "Goats", "Zombies", "Rich People", "Swimmers", "Florida", "Vampires", "Dragons", "Internet of Things", "Mars", "Cryptocurrency", "Mosquito Repellent", "Fidget Spinners", "Sun Screen", "Water Bottles", "Lost Travelers", "Superheroes"]
+    
+    @IBOutlet weak var startupLabel: UILabel!
+    @IBOutlet weak var ideaLabel: UILabel!
+    @IBOutlet weak var ideaGeneratorButton: UIButton!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         
         view.gradientBG(colorOne: .lightPink, colorTwo: .lightPurple)
         
+    }
+    
+    @IBAction func ideaGeneratorTapped(_ sender: Any) {
+        
+        generateIdeas(list: startups, label: startupLabel)
+        generateIdeas(list: ideas, label: ideaLabel)
+        
+    }
+    
+    func generateIdeas(list: [String], label: UILabel) {
+        let indexes = UInt32(list.count)
+        let randomIndex = Int(arc4random_uniform(indexes))
+        label.text = list[randomIndex]
+
     }
     
 }
